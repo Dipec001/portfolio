9068f56553fb004a82ca458a59bff8fe2fd78ada
Dipec001 <dpecchukwu@gmail.com>
2024-07-27 13:34:47 +0100

    "Removed pip upgrade and install commands from build script, updated Django and asgiref versions in requirements.txt"

--- build.sh
+++ build.sh
@@ -2,12 +2,6 @@
 # Exit on error
 set -o errexit
 
-# Upgrade pip to the latest version
-/opt/render/project/src/.venv/bin/python3.7 -m pip install --upgrade pip
-
-# Install project dependencies
-/opt/render/project/src/.venv/bin/python3.7 -m pip install -r requirements.txt
-
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
